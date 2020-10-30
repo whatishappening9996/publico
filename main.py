d1172139e6e60075cf61e0941f9ad444b5037cc0
whatishappening9996 <60358987+whatishappening9996@users.noreply.github.com>
2020-10-30 15:21:33 -0400

    slight

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import streamlit as sl
 import time
 
 fil = sl.file_uploader(label='Upload an image')
-txt = sl.text_input(label='Or copy/paste a med list here')
+txt = sl.text_input(label='Or copy/paste a list here')
 btn = sl.button(label= 'Analyze')
+
+if txt and btn:
+    
